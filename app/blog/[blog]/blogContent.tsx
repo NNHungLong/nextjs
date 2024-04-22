6afeaee31320774c88175e31327fab8973e1fc4a
NNHungLong <ng2hunglong@gmail.com>
2024-04-22 22:29:32 +0700

    fix code block markdown

--- app/blog/[blog]/blogContent.tsx
+++ app/blog/[blog]/blogContent.tsx
@@ -1,3 +1,10 @@
+"use client"
+import { createRef } from 'react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import { Post } from '@/.contentlayer/generated';
 import { format, parseISO } from 'date-fns'
 
@@ -6,12 +13,39 @@ interface BlogContentProps {
 }
 
 export default function BlogContent({ post }: BlogContentProps) {
+  const syntaxHighlighterRef = createRef<SyntaxHighlighter>();
   return (
     <div className='flex flex-col'>
       <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <Markdown
+        className='markdown-body'
+        remarkPlugins={[[remarkGfm]]}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || '');
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag='div'
+                language={match[1]}
+                style={coldarkDark}
+                ref={syntaxHighlighterRef}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {post?.body?.raw}
+      </Markdown>
     </div>
   );
 }
